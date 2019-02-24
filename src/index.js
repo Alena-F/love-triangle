b3b0f74d4b6e71329bdb68977e97d7570f4b10f7
Alena-F <alena_fetisova@mail.ru>
2019-02-24 22:03:32 +0300

    task 2 complited

--- src/index.js
+++ src/index.js
@@ -3,5 +3,31 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let newMassive = [];
+  for (let i=0; i<preferences.length; i++) {
+    let k = preferences[i];
+    let m = preferences[k-1];
+    let n = preferences[m-1];
+    let z = preferences[n-1];
+    if (z==k) {
+      newMassive[newMassive.length] = k;
+      newMassive[newMassive.length] = m;
+      newMassive[newMassive.length] = n;  
+      }
+  }
+  function unique(arr) {
+    let result = [];
+  
+    nextInput:
+      for (let y = 0; y < arr.length; y++) {
+        let str = arr[y]; 
+        for (let j = 0; j < result.length; j++) { 
+          if (result[j] == str) continue nextInput; 
+        }
+        result.push(str);
+      }
+  
+    return Math.floor(result.length/3);
+  }
+   return unique(newMassive);
 };
